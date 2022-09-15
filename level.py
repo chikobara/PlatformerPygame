198d68a5286791afc24fb3cf9c7c977342c526d7
Chikobara <anonymoussyria6@gmail.com>
2022-09-15 14:11:06 +0300

    A

---
+++ level.py
@@ -0,0 +1,15 @@
+import pygame
+from tiles import Tile
+
+
+class Level:
+    def __init__(self, level_data, surface):
+        self.display_surface = surface
+        self.setup_level(level_data)
+
+    def setup_level(self, layout):
+        self.tiles = pygame.sprite.Group()
+        
+
+    def run(self):
+        
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pygame, sys
 from settings import *
+from tiles import Tile
+from level import Level
+
 
 pygame.init()
 # pygame setup
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
+level = Level(level_map, screen)
 
 while True:
     for event in pygame.event.get():
@@ -13,6 +17,5 @@ while True:
             pygame.quit()
             sys.exit()
     screen.fill("black")
-
     pygame.display.update()
     clock.tick(60)
