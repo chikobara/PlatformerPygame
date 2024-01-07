d3e2255a87d2e00224468396ada3d41262222326
Chikobara <albaraa.as.2002@gmail.com>
2024-01-07 16:19:00 +0300

    update

--- level.py
+++ level.py
@@ -1,5 +1,6 @@
 import pygame
 from tiles import Tile
+from settings import tile_size, screen_width
 
 
 class Level:
@@ -9,6 +10,15 @@ class Level:
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
+        for row_index, row in enumerate(layout):
+            for col_index, cell in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
+
+                if cell == "X":
+                    tile = Tile((x, y), tile_size)
+                    self.tiles.add(tile)
+                # if cell == 'P'
 
     def run(self):
-        pass
+        self.tiles.draw(self.display_surface)
--- main.py
+++ main.py
@@ -11,11 +11,14 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 clock = pygame.time.Clock()
 level = Level(level_map, screen)
 
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
     screen.fill("black")
+    level.run()
+
     pygame.display.update()
     clock.tick(60)
